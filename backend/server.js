ad01e2604389df12ee416c67fede29d064062924
Sowmik Dey <sowmikdey23@gmail.com>
2025-02-04 01:18:28 +0530

    backend link updated

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://uwu-codegen-frontend-nyfg.onrender.com',
   credentials: true
 }));
 app.use(bodyParser.json());
