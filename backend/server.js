9dbaaac367887d1de87c0273083c9738c3d2f67a
Sowmik Dey <sowmikdey23@gmail.com>
2025-02-05 17:20:46 +0530

    build command added

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: 'https://uwu-codegen-frontend-nyfg.onrender.com',
+  origin: '',
   credentials: true
 }));
 app.use(bodyParser.json());
